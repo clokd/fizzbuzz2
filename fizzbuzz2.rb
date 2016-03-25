f0d0caf4d3b77af796af788df8966c1aa764fe5a
Stuart Williamson <stuart.williamson@mac.com>
2016-03-25 12:19:14 +0000

    Second iteration of the fizzbuzz exercise. Had to use a literal.

--- fizzbuzz2.rb
+++ fizzbuzz2.rb
@@ -1,13 +1,12 @@
-n = 1
-while n <= 100
-	if n % 15 == 0
+array = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40]
+array.each do |x|
+	if x % 15 == 0
 		puts "FizzBuzz"
-	elsif n % 3 == 0 
+	elsif x % 3 == 0
 		puts "Fizz"
-	elsif n % 5 == 0
+	elsif x % 5 ==0
 		puts "Buzz"
-	else 
-		puts "#{n}"
-	end
-  n = n + 1
+	else
+	puts "#{x}"
+end
 end
